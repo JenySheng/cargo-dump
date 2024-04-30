a858bd959b7c3f1b827cfd98eff00b5b695ab528
GuLixuan <143043541+GuLixuan@users.noreply.github.com>
2024-04-29 22:47:46 -0500

    Add files via upload

--- main.rs
+++ main.rs
@@ -1,52 +1,81 @@
 use yew::prelude::*;
 
-struct Model {
-    value: i64,
-}
-
 #[function_component(App)]
 fn app() -> Html {
-    let state = use_state(|| Model { value: 0 });
+    let modal_title = use_state(|| "".to_string());
+    let show_modal = use_state(|| false);
 
-    // need to be changed to the code that help us open the "choosing cards" page
-    let onclick = {
-        let state = state.clone();
-
-        Callback::from(move |_| {
-            state.set(Model {
-                value: state.value + 1,
-            })
-        })
+    let toggle_modal_with_title = {
+        let modal_title = modal_title.clone();
+        let show_modal = show_modal.clone();
+        move |title: String| {
+            if *show_modal && *modal_title == title {
+                show_modal.set(false);
+            } else {
+                modal_title.set(title);
+                show_modal.set(true);
+            }
+        }
     };
 
-    let buttons: Html = (0..5).map(|_| {
-        html! {
-            <button onclick={onclick.clone()}>{"+"}</button>
-        }
-    })
-    .collect();
+    let modal_content = html! {
+        <div class="modal-content">
+            <p>{&*modal_title}</p>
+            <p>{"♣️  2   3   4   5   6   7   8   9   10   J   Q   K   A"}</p>
+            <p>{"♦️  2   3   4   5   6   7   8   9   10   J   Q   K   A"}</p>
+            <p>{"♥️  2   3   4   5   6   7   8   9   10   J   Q   K   A"}</p>
+            <p>{"♠️  2   3   4   5   6   7   8   9   10   J   Q   K   A"}</p>
+        </div>
+    };
+    
 
-    let holes: Html = (0..2).map(|_| {
+    let modal = if *show_modal {
         html! {
-            <button class="holes" onclick={onclick.clone()}>
-                <span>{"+"}</span>
-            </button>
+            <div class="modal" onclick={move |_| show_modal.set(false)}>
+                {modal_content}
+            </div>
         }
-    })
-    .collect();
+    } else {
+        html! {}
+    };
+
+    let buttons_top_row: Html = (1..=5).map(|n| {
+        let toggle_modal = toggle_modal_with_title.clone();
+        html! {
+            <button class="top-row-button" onclick={move |_| {
+                let title = match n {
+                    1 | 2 | 3 => "Pick the Flop cards",
+                    4 => "Pick the Turn card",
+                    5 => "Pick the River card",
+                    _ => "Pick the Card", 
+                };
+                toggle_modal(title.to_string())
+            }}>{ "+" }</button>
+        }
+    }).collect();
+
+    let buttons_bottom_row: Html = (1..=2).map(|_| {
+        let toggle_modal = toggle_modal_with_title.clone();
+        html! {
+            <button class="bottom-row-button" onclick={move |_| toggle_modal("Pick the Hole Cards".to_string())}>{ "+" }</button>
+        }
+    }).collect();
 
     html! {
-        <div>
+        <div id="background">
             <div id="title">
                 {"Texas Hold'em Poker"}
             </div>
-            <div id="buttons-containers">
-                {buttons}
+            <div id="top-row">
+                {buttons_top_row}
             </div>
-            <div id="holes-containers">
-                {holes}
+            <div id="bottom-row">
+                {buttons_bottom_row}
             </div>
-            <p>{state.value}</p>
+            <div id="current-card-info">
+                {"Current cards:"}
+            </div>
+            {modal}
         </div>
     }
 }
